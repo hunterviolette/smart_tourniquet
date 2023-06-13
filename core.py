ffb0eafaa039103ecfbe08ff60789cae4bf83db2
hunterviolette <hunter@maplevalleypharms.com>
2023-06-13 18:18:41 -0400

    clean formatting

--- core.py
+++ core.py
@@ -7,26 +7,18 @@ from i2c_lcd import I2cLcd
 import time
 from machine import ADC, Pin
 
- 
-
 I2C_ADDR = 0x27
 totalRows = 2
 totalColumns = 16
-
- 
 
 #setup the Pulse Sensor reading pin
 analog_pulse=machine.ADC(28)
 led = Pin(25, Pin.OUT)
 
- 
-
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, totalRows, totalColumns) 
 pump = Pin(20, Pin.OUT)
 pump(0)
-
- 
 
 def CollectData(startTime, stopTime):
     while time.ticks_ms() < stopTime:
@@ -38,24 +30,14 @@ def CollectData(startTime, stopTime):
         lcd.putstr("volts: " + str(round(voltage_value, 3)))
         
 
- 
-
 def CalcBPM(data, timeDelta, window, subgrouping, threshold):
 
- 
-
     m = [[y[0][i-subgrouping:i+subgrouping], y[1][i-subgrouping:i+subgrouping]]  
-         for i, y in enumerate(data) if y[1][i-subgrouping:i+6]]     
-
- 
+        for i, y in enumerate(data) if y[1][i-subgrouping:i+6]]     
 
     maxList = [[x[0][x[1].index(max(x[1]))], max(x[1])] for x in m]
 
- 
-
-
     #final = set({tuple(i) for i in maxList})
-    
     
     a = [[x[0], x[1]] for x in list(set({tuple(i) for i in maxList}))]
     final = [x for x in a if x[0] > (max([x[0] for x in a]) - window)]
@@ -71,17 +53,13 @@ def CalcBPM(data, timeDelta, window, subgrouping, threshold):
     
     return bpm
 
- 
-
 def ProcessData():
     time, voltage = [], []
     for i, x in enumerate(rawData):
-      time.append(rawData[i][0])
-      voltage.append(rawData[i][1])
-  
-    return [[time, voltage] for x in voltage]
+        time.append(rawData[i][0])
+        voltage.append(rawData[i][1])
 
- 
+    return [[time, voltage] for x in voltage]
 
 if __name__ == '__main__':
     
@@ -94,8 +72,6 @@ if __name__ == '__main__':
     startTime = time.ticks_ms()
     stopTime = startTime + (window*1000) #time change original 60*1000 (60 secs)
     timeDelta = (stopTime - startTime)/1000
-
- 
 
     while True:
         stopTime = time.ticks_ms() + (10*1000)
